d67e419b9a1bcc7a458f201d729a2bfc90d8f52e
rajeshfcgss <rajeshgunait@gmail.com>
2022-09-28 19:54:44 +0530

    modification add1 commit

--- angular-app-heroku/src/app/app.component.ts
+++ angular-app-heroku/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularraj-app-heroku';
+  title = 'angularraj1-app-heroku';
 }
